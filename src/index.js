05b4e11969dbb0d7b681e5e17e13acd88713d1f1
Isabel de la Cuadra <isabel.de.la.cuadra@gmail.com>
2023-06-16 22:36:02 -0400

    Proyecto de Isabel de la Cuadra

--- src/index.js
+++ src/index.js
@@ -38,38 +38,72 @@ let persona4 = {
 }
 
 
-let personas = []
+var personas = []
 
 personas.push(persona1, persona2, persona3, persona4)
-// console.log(personas);
+    // console.log(personas);
 
 // console.log(personas);
 
 
 const incluirFechas = () => {
     for (let index = 0; index < personas.length; index++) {
-        personas[index] = { ...fechas, ...personas[index]};
+        personas[index] = {...fechas, ...personas[index] };
     }
 }
 incluirFechas()
 console.log(personas);
-
-
 
 const listadoPersonas = (listado) => {
     for (const itemPersona of listado) {
         const { rut, nombre, apellido } = itemPersona
         const mensaje = validate(rut) ? 'Si' : 'No'
         console.log(`${format(rut)} | ${nombre} | ${apellido} | Válido: ${mensaje}`);
-
-
-
     }
 
     // Opción2
     // for (const { rut, nombre, apellido } of listado) {
-    //     // const { rut, nombre, apellido } = itemPersona
     //     console.log(`${format(rut)} | ${nombre} | ${apellido} | Válido: ${validate(rut)}`);
     // }
 }
 listadoPersonas(personas)
+
+/* 
+- instalar dependencias. npm install
+- Levantar el proyecto npx nodemon
+- Desarrollar una función que reciba cómo parámetro una fecha, validar que la fecha es del futuro si no es del futuro mostrar
+  un mensaje que indique que la fecha es inválida. En caso de ser una fecha del futuro mostrar un listado de las personas e
+  indicar la edad que tendrán para esa fecha.
+*/
+
+function esFechaDelFuturo(fecha) {
+    const fechaActual = moment();
+    return moment(fecha).isAfter(fechaActual);
+}
+
+function calcularEdades(personas, fecha) {
+    if (!esFechaDelFuturo(fecha)) {
+        console.log('La fecha es inválida. Debe ser una fecha del futuro.');
+        return;
+    }
+
+    mostrarEdadFutura(personas)
+}
+
+function mostrarEdadFutura(personas) {
+    personas.forEach(persona => {
+        const edad = moment(fecha).diff(persona.fecha_nacimiento, 'years');
+        console.log(`${persona.nombre} tendrá ${edad} años para la fecha ${invertirFecha(fecha)}`);
+    });
+
+}
+
+function invertirFecha(fecha) {
+    var partes = fecha.split("-");
+    var fechaInvertida = partes.reverse().join("-");
+    return fechaInvertida;
+}
+
+const fecha = '2030-01-01';
+
+calcularEdades(personas, fecha);
